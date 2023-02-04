9f78837fc402df988f1f36c51ae85c1e77004ba6
CllDMR <celal.demirx@gmail.com>
2023-02-04 23:43:44 +0300

    feat(blog): :lipstick: added tailwind css

---
+++ apps/blog/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "../../node_modules/ui-react/src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
