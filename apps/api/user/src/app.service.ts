0b72090d5f5677fb547002e2ed25832da5e39067
CllDMR <celal.demirx@gmail.com>
2023-06-17 20:18:49 +0300

    feat(api-user): :sparkles: crud service methods

--- apps/api/user/src/app.service.ts
+++ apps/api/user/src/app.service.ts
@@ -2,7 +2,23 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  list(): string {
+    return 'Hello World!';
+  }
+
+  create(): string {
+    return 'Hello World!';
+  }
+
+  read(): string {
+    return 'Hello World!';
+  }
+
+  update(): string {
+    return 'Hello World!';
+  }
+
+  delete(): string {
     return 'Hello World!';
   }
 }
