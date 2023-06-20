662f5ef65adc3561d5744d172d22f5e06fe98669
CllDMR <celal.demirx@gmail.com>
2023-06-20 12:12:06 +0300

    fix: :bug: fix spec errors

--- apps/api/todo/src/app.controller.spec.ts
+++ apps/api/todo/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return empty array', () => {
+      expect(appController.list({ params: {}, query: {} })).toStrictEqual([]);
     });
   });
 });
