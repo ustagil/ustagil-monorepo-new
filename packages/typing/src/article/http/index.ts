d6a4d00d477fe43944d24258c303a75cf69e59b9
CllDMR <celal.demirx@gmail.com>
2023-06-23 15:56:11 +0300

    feat(typing): :label: added http types of article and user

--- packages/typing/src/article/http/index.ts
+++ packages/typing/src/article/http/index.ts
@@ -1,1 +1,22 @@
-export {};
+export type ArticleListDto = {
+  query: any;
+};
+
+export type ArticleReadDto = {
+  query: any;
+  params: { id: string };
+};
+
+export type ArticleCreateDto = {
+  query: any;
+  body: any;
+};
+
+export type ArticleUpdateDto = {
+  params: { id: string };
+  body: any;
+};
+
+export type ArticleDeleteDto = {
+  params: { id: string };
+};
--- packages/typing/src/user/http/index.ts
+++ packages/typing/src/user/http/index.ts
@@ -1,1 +1,22 @@
-export {};
+export type UserListDto = {
+  query: any;
+};
+
+export type UserReadDto = {
+  query: any;
+  params: { id: string };
+};
+
+export type UserCreateDto = {
+  query: any;
+  body: any;
+};
+
+export type UserUpdateDto = {
+  params: { id: string };
+  body: any;
+};
+
+export type UserDeleteDto = {
+  params: { id: string };
+};
