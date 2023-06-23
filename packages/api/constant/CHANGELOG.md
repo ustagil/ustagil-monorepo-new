3fdc9cd25450341ba077b3245f4f94093333cfa9
CllDMR <celal.demirx@gmail.com>
2023-06-23 15:03:01 +0300

    feat(api-constant): :sparkles: regenerated

--- packages/api/constant/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# @ustagil/api-constant
-
-## 0.0.1
-
-### Patch Changes
-
-- reset and init versions
--- packages/api/constant/src/index.ts
+++ packages/api/constant/src/index.ts
@@ -1,16 +1,16 @@
-export const API_ARTICLE_QUERY_MS = "ARTICLE_QUERY";
-export const API_ARTICLE_COMMAND_MS = "ARTICLE_COMMAND";
-export const API_ARTICLE_GROUP_ID = "article-consumer";
-export const API_ARTICLE_CLIENT_ID = "article";
+export const API_ARTICLE_QUERY_MS = 'ARTICLE_QUERY';
+export const API_ARTICLE_COMMAND_MS = 'ARTICLE_COMMAND';
+export const API_ARTICLE_GROUP_ID = 'article-consumer';
+export const API_ARTICLE_CLIENT_ID = 'article';
 
-export const API_USER_QUERY_MS = "USER_QUERY";
-export const API_USER_COMMAND_MS = "USER_COMMAND";
-export const API_USER_GROUP_ID = "user-consumer";
-export const API_USER_CLIENT_ID = "user";
+export const API_USER_QUERY_MS = 'USER_QUERY';
+export const API_USER_COMMAND_MS = 'USER_COMMAND';
+export const API_USER_GROUP_ID = 'user-consumer';
+export const API_USER_CLIENT_ID = 'user';
 
-export const API_TODO_QUERY_MS = "TODO_QUERY";
-export const API_TODO_COMMAND_MS = "TODO_COMMAND";
-export const API_TODO_GROUP_ID = "todo-consumer";
-export const API_TODO_CLIENT_ID = "todo";
+export const API_TODO_QUERY_MS = 'TODO_QUERY';
+export const API_TODO_COMMAND_MS = 'TODO_COMMAND';
+export const API_TODO_GROUP_ID = 'todo-consumer';
+export const API_TODO_CLIENT_ID = 'todo';
 
-export const API_KAFKA_BROKER = "localhost:9092";
+export const API_KAFKA_BROKER = 'localhost:9092';
