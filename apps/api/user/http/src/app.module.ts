22c2ea73710576e5237cbe77ade0c285cee5ce31
CllDMR <celal.demirx@gmail.com>
2023-06-26 19:03:07 +0300

    feat: :sparkles: implemented authn to all http-gateways

--- apps/api/user/http/src/app.module.ts
+++ apps/api/user/http/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import {
   API_KAFKA_BROKER,
@@ -8,7 +10,9 @@ import {
   API_USER_COMMAND_MS,
   API_USER_GROUP_ID,
   API_USER_QUERY_MS,
+  JWT_SECRET,
 } from '@ustagil/api-constant';
+import { BaseJwtStrategy, JwtAuthGuard } from '@ustagil/api-util';
 import { join } from 'path';
 import { AppController } from './app.controller';
 
@@ -41,8 +45,20 @@ import { AppController } from './app.controller';
         },
       },
     ]),
+    JwtModule.register({
+      global: true,
+      secret: JWT_SECRET,
+      signOptions: { expiresIn: '60s' },
+    }),
   ],
   controllers: [AppController],
-  providers: [],
+  providers: [
+    BaseJwtStrategy,
+    JwtService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
