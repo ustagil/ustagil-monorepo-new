8c169a881e1243230c1f9150251d28db6fe2fb8c
CllDMR <celal.demirx@gmail.com>
2023-07-02 16:49:27 +0300

    fix(api-todo-command): :art: removed unnecessary model DI

--- apps/api/todo/command/src/todo/todo.controller.ts
+++ apps/api/todo/command/src/todo/todo.controller.ts
@@ -1,24 +1,18 @@
 import { Controller } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import { MessagePattern } from '@nestjs/microservices';
-import { InjectModel } from '@nestjs/mongoose';
 import {
   TodoKafkaCreateRequest,
   TodoKafkaDeleteRequest,
   TodoKafkaUpdateRequest,
 } from '@ustagil/typing';
-import { Model } from 'mongoose';
 import { CreateTodoCommand } from './create-todo/create-todo.command';
 import { DeleteTodoCommand } from './delete-todo/delete-todo.command';
-import { TodoModel } from './todo.schema';
 import { UpdateTodoCommand } from './update-todo/update-todo.command';
 
 @Controller()
 export class TodoController {
-  constructor(
-    @InjectModel(TodoModel.name) private todoModel: Model<TodoModel>,
-    private commandBus: CommandBus,
-  ) {}
+  constructor(private commandBus: CommandBus) {}
 
   @MessagePattern('todo.create')
   async create(dto: TodoKafkaCreateRequest) {
