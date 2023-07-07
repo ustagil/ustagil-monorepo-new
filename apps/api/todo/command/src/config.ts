54ebc002b86402f00ae536ccf94bf30c53bc04fa
CllDMR <celal.demirx@gmail.com>
2023-07-07 16:44:54 +0300

    fix(api-todo-command): :bug: added eventstore uri to config

--- apps/api/todo/command/src/config.ts
+++ apps/api/todo/command/src/config.ts
@@ -2,6 +2,7 @@ import { ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
 
 type MyConfig = {
+  API_TODO_EVENTSTOREDB_URI: string;
   API_TODO_KAFKA_BROKER: string;
   API_TODO_KAFKA_GROUP_ID: string;
   API_TODO_MONGODB_URI: string;
@@ -10,6 +11,7 @@ type MyConfig = {
 export type MyConfigService = ConfigService<MyConfig, true>;
 
 export const validateEnvConfig = Joi.object<MyConfig, true>({
+  API_TODO_EVENTSTOREDB_URI: Joi.string(),
   API_TODO_KAFKA_BROKER: Joi.string(),
   API_TODO_KAFKA_GROUP_ID: Joi.string(),
   API_TODO_MONGODB_URI: Joi.string(),
