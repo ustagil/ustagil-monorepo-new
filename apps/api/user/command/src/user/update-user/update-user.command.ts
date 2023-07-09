e048fec78c3ad9fb6b9054fe0104ed2625836b36
CllDMR <celal.demirx@gmail.com>
2023-07-09 19:33:20 +0300

    refactor: :fire: removed old user app

--- apps/api/user/command/src/user/update-user/update-user.command.ts
+++
@@ -1,7 +0,0 @@
-export class UpdateUserCommand {
-  constructor(
-    public readonly id: string,
-    public readonly username?: string | undefined,
-    public readonly password?: string | undefined,
-  ) {}
-}
